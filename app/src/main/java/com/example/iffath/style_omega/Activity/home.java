ee8db02b6f2899dbcdc301944514bee3f7741473
Shadiya Iffath <shadiya.iffath98@gmail.com>
2019-08-30 02:17:09 +0530

    before-gradle-update

--- app/src/main/java/com/example/iffath/style_omega/Activity/home.java
+++ app/src/main/java/com/example/iffath/style_omega/Activity/home.java
@@ -1,16 +1,42 @@
 package com.example.iffath.style_omega.Activity;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.ImageButton;
 
 import com.example.iffath.style_omega.R;
+import com.google.android.material.navigation.NavigationView;
 
 public class home extends AppCompatActivity {
+    private DrawerLayout drawerLayout;
+    private ActionBarDrawerToggle actionBarDrawerToggle;
+    private boolean drawer_state;
+    private NavigationView navDrawer;
+    ImageButton hamMenu;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+//
+//        hamMenu = findViewById(R.id.btnMenu);
+//        navDrawer = findViewById(R.id.navView);
+//
+//        hamMenu.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                DrawerLayout navDrawer = findViewById(R.id.home_drawer);
+//                // If the navigation drawer is not open then open it, if its already open then close it.
+//                if(!navDrawer.isDrawerOpen(GravityCompat.START)) navDrawer.openDrawer(GravityCompat.START);
+//                else navDrawer.closeDrawer(GravityCompat.START, false);
+//            }
+//        });
     }
 }
